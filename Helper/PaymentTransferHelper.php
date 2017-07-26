4d1ec42d2c9985470248027cc9ada9201ace5c6a
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2017-07-26 14:52:45 +0200

    transfer helper

---
+++ Helper/PaymentTransferHelper.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Troopers\MangopayBundle\Helper;
+
+use MangoPay\Money;
+use MangoPay\PayOut;
+use MangoPay\PayOutPaymentDetailsBankWire;
+use MangoPay\Transfer;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Troopers\MangopayBundle\Entity\BankInformationInterface;
+use Troopers\MangopayBundle\Entity\TransactionInterface;
+
+/**
+ * ref: troopers_mangopay.payment_transfer_helper.
+ **/
+class PaymentTransferHelper
+{
+    /**
+     * @var MangopayHelper
+     */
+    private $mangopayHelper;
+
+    /**
+     * @var WalletHelper
+     */
+    private $walletHelper;
+
+    /**
+     * PaymentOutHelper constructor.
+     * @param MangopayHelper $mangopayHelper
+     * @param WalletHelper $walletHelper
+     */
+    public function __construct(MangopayHelper $mangopayHelper, WalletHelper $walletHelper)
+    {
+        $this->mangopayHelper = $mangopayHelper;
+        $this->walletHelper = $walletHelper;
+    }
+
+    /**
+     * @param TransactionInterface $transfer
+     * @return Transfer
+     */
+    public function createTransfer(TransactionInterface $transfer)
+    {
+        $debitedWallet = $transfer->getDebitedWallet();
+        $creditedWallet = $transfer->getCreditedWallet();
+        $currency = $debitedWallet->getCurrencyCode();
+
+        $debitedFunds = $this->buildMoney($transfer->getDebitedFunds(), $currency);
+        $fees = $this->buildMoney($transfer->getFees(), $currency);
+
+        $mangoTransfer = new Transfer();
+        $user = $debitedWallet->getUser();
+        $mangoTransfer->AuthorId = $user->getMangoUserId();
+        $mangoTransfer->DebitedFunds = $debitedFunds;
+        $mangoTransfer->Fees = $fees;
+        $mangoTransfer->DebitedWalletId = $debitedWallet->getMangoWalletId();
+        $mangoTransfer->CreditedWalletId = $creditedWallet->getMangoWalletId();
+
+        return $this->mangopayHelper->Transfers->Create($mangoTransfer);
+    }
+
+    /**
+     * @param string $amount
+     * @param string $currency
+     * @return Money
+     */
+    public function buildMoney($amount = '0', $currency = 'EUR')
+    {
+        $money = new Money();
+        $money->Currency = $currency;
+        $money->Amount = $amount;
+
+        return $money;
+    }
+}
